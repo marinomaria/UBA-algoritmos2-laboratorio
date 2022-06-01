5e62f9628b56d50b91b0acf4c57254a3ad6e5825
María Marino <mariamarino@Marias-MacBook-Air.local>
2022-06-01 16:10:54 -0300

    Agrego archivos del taller Trie

---
+++ g4/taller/src/string_map.hpp
@@ -0,0 +1,53 @@
+template <typename T>
+string_map<T>::string_map(){
+    // COMPLETAR
+}
+
+template <typename T>
+string_map<T>::string_map(const string_map<T>& aCopiar) : string_map() { *this = aCopiar; } // Provisto por la catedra: utiliza el operador asignacion para realizar la copia.
+
+template <typename T>
+string_map<T>& string_map<T>::operator=(const string_map<T>& d) {
+    // COMPLETAR
+}
+
+template <typename T>
+string_map<T>::~string_map(){
+    // COMPLETAR
+}
+
+template <typename T>
+T& string_map<T>::operator[](const string& clave){
+    // COMPLETAR
+}
+
+
+template <typename T>
+int string_map<T>::count(const string& clave) const{
+    // COMPLETAR
+}
+
+template <typename T>
+const T& string_map<T>::at(const string& clave) const {
+    // COMPLETAR
+}
+
+template <typename T>
+T& string_map<T>::at(const string& clave) {
+    // COMPLETAR
+}
+
+template <typename T>
+void string_map<T>::erase(const string& clave) {
+    // COMPLETAR
+}
+
+template <typename T>
+int string_map<T>::size() const{
+    // COMPLETAR
+}
+
+template <typename T>
+bool string_map<T>::empty() const{
+    // COMPLETAR
+}
